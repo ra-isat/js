fd5051b7e12438398bacd27c0877ad8df7b9e26f
Raisat Sirazhudinova <raisat13666@gmail.com>
2020-03-31 00:31:22 +0300

    lesson02

---
+++ script/script.js
@@ -0,0 +1,16 @@
+let money = 40000;
+let income = 10000;
+let addExpenses = ("интернет, Дорога, комуналка, Еда");
+let deposit = true;
+let mission = 100000;
+let period = 12;
+
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase());
+console.log(2);
+
+let budgetDay = (money / 30);
+console.log(budgetDay);
