49421f642b3fe845474df7f1c0a8da08b3c2b56a
Raisat Sirazhudinova <raisat13666@gmail.com>
2020-03-31 09:09:24 +0300

    string - array

--- script/script.js
+++ script/script.js
@@ -9,8 +9,10 @@ console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
-console.log(addExpenses.toLowerCase());
-console.log(2);
+
+addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
+
 
 let budgetDay = (money / 30);
 console.log(budgetDay);
