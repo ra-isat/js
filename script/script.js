688066c9f2e4f0d6fdbd9fb69b8291df89f4ef4b
Raisat Sirazhudinova <raisat13666@gmail.com>
2020-03-31 19:36:32 +0300

    lesson03

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+'use strict';
 let money = 40000;
 let income = 10000;
 let addExpenses = ("интернет, Дорога, комуналка, Еда");
@@ -16,3 +17,36 @@ console.log(addExpenses.split(', '));
 
 let budgetDay = (money / 30);
 console.log(budgetDay);
+
+money = prompt('Ваш месячный доход?', '50000');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую')
+console.log(addExpenses.split(', '));
+
+deposit = confirm('Есть ли у вас депозит в банке?');
+console.log(deposit);
+
+let expenses1 = prompt('Введите обязательную статью расходов?');
+console.log(expenses1);
+let amount1 = prompt('Во сколько это обойдется?');
+
+let expenses2 = prompt('Введите обязательную статью расходов?');
+console.log(expenses2);
+let amount2 = prompt('Во сколько это обойдется?');
+
+let budgetMonth = (money - (+amount1 + (+amount2)));
+
+console.log(budgetMonth);
+console.log('Цель будет достигнута за ' + Math.ceil(mission / budgetMonth) + ' месяцев');
+
+budgetDay = Math.floor(budgetMonth / 30);
+console.log('За день я могу потратить ' + budgetDay + 'р.');
+
+if (budgetDay > 1200) {
+    alert('У вас высокий уровень дохода');
+} else if (600 <= budgetDay <= 1200) {
+    alert('У вас средний уровень дохода');
+} else if (budgetDay < 600) {
+    alert('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay <= 0) {
+    alert('Что то пошло не так');
+}
